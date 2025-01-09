45c815caf6bd582a7954aab9b0ac1f967a6b09e3
Nicholas Rosati <nicholas@rosati.xyz>
2025-01-09 10:43:41 -0500

    Initial commit 2

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
 #include "C:/Program Files (x86)/LabJack/Drivers/LabJackUD.h"
 
-int main(void) {
+int main() {
     printf("Driver version: %f\n", GetDriverVersion());
     return 0;
 }

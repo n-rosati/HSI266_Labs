4cee885a1c0cbde3ff7c769f03ef1d00b69dd9e9
Nicholas Rosati <nicholas@rosati.xyz>
2025-03-04 22:37:31 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "C:/Program Files (x86)/LabJack/Drivers/LabJackUD.h"
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}

97f080679bd7de87b3b0b4e606c26c49dfc9999a
Nicholas Rosati <nicholas@rosati.xyz>
2025-03-04 23:44:03 -0500

    add menu item 2 functionality (untested)

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
     LJ_HANDLE lj_handle = 0;
     LJ_ERROR lj_cue = 0;
     double ljSN = 0;
-    char userInput, userInputMenu1, userInputMenu2;
+    char userInputMenu1 = '3', userInputMenu2 = '7';
     double btnFIO4, potAIN0;
 
     lj_cue = OpenLabJack(LJ_dtU3, LJ_ctUSB, "1", 1, &lj_handle);
@@ -26,7 +26,67 @@ int main() {
     lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 65535, 0, 0);
     lj_cue = Go();
 
+    do {
+        printf("1. Colour adjusts with potentiometer\n"
+               "2. Manual colour choice.\n"
+               "3. Exit\n"
+               "Enter a number:\n");
+        scanf_s("%d\n", &userInputMenu1);
 
+        switch (userInputMenu1) {
+            case '1':
+
+                break;
+            case '2':
+                do {
+                    printf("1. Red\n2. Green.\n3. Blue\n4. Violet\n5. Yellow\n6.White\n7. Back to main menu\nEnter a number:");
+                    scanf_s("%d\n", &userInputMenu2);
+                    switch (userInputMenu2) {
+                        case '1': // Red
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 0, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 65535, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_DAC, 0, 0, 0, 0);
+                            lj_cue = Go();
+                            break;
+                        case '2': // Green
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 65535, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 0, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_DAC, 0, 0, 0, 0);
+                            lj_cue = Go();
+                            break;
+                        case '3': // Blue
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 65535, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 65535, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_DAC, 0, 3.3, 0, 0);
+                            lj_cue = Go();
+                            break;
+                        case '4': // Violet
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 0, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 65535, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_DAC, 0, 3.3, 0, 0);
+                            lj_cue = Go();
+                            break;
+                        case '5': // Yellow
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 0, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 65535, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_DAC, 0, 0, 0, 0);
+                            lj_cue = Go();
+                            break;
+                        case '6': // White
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 0, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 1, 0, 0, 0);
+                            lj_cue = AddRequest(lj_handle, LJ_ioPUT_DAC, 0, 3.3, 0, 0);
+                            lj_cue = Go();
+                            break;
+                        default:
+                            break;
+                    }
+                } while (userInputMenu2 != '7');
+                break;
+            default:
+                break;
+        }
+    } while (userInputMenu1 != '3');
 
     // Turn off the LED and quit
     lj_cue = AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 65535, 0, 0);

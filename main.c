e04d7c6015cd21971719819b91897b1c03076f03
Nicholas Rosati <nicholas@rosati.xyz>
2025-03-13 23:06:30 -0400

    done

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* Name: Nicholas Rosati
+ * Program name: HSI266 Lab 7
+ * Date 2025-03-13
+ * Purpose: The program starts by printing the connected LabJack serial number to the console.
+ *          Next, the program will run and do nothing until the ambient light sensor, connected to AIN0, detects darkness.
+ *          When darkness is detected, FIO5 is set high which will activate a relay that energizes a solenoid.
+ *          The solenoid will then block an optical sensor (connected to AIN1). When the optical sensor is blocked,
+ *          FIO4 will output a 3Hz 50% duty cycle PWM signal to flash an LED.
+ *          When the ambient light sensor detects light again, the solenoid is de-energized and the LED is turned off.
+ *          This series of events can happen 3 times before the program ends.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
@@ -18,7 +30,7 @@ int main() {
 
     // Set up initial timer
     AddRequest(lj_handle, LJ_ioPUT_CONFIG, LJ_chNUMBER_TIMERS_ENABLED, 1, 0, 0);
-    AddRequest(lj_handle, LJ_ioPUT_CONFIG, LJ_chTIMER_COUNTER_PIN_OFFSET, 5, 0, 0);
+    AddRequest(lj_handle, LJ_ioPUT_CONFIG, LJ_chTIMER_COUNTER_PIN_OFFSET, 4, 0, 0);
     AddRequest(lj_handle, LJ_ioPUT_CONFIG, LJ_chTIMER_CLOCK_BASE, LJ_tc1MHZ_DIV, 0, 0);
     AddRequest(lj_handle, LJ_ioPUT_CONFIG, LJ_chTIMER_CLOCK_DIVISOR, 5, 0, 0);
     AddRequest(lj_handle, LJ_ioPUT_TIMER_MODE, 0, LJ_tmPWM16, 0, 0);
@@ -27,25 +39,29 @@ int main() {
 
     while (count < 3){
         AddRequest(lj_handle, LJ_ioGET_AIN, 0, 0, 0, 0);
-        AddRequest(lj_handle, LJ_ioGET_AIN, 1, 0, 0, 0);
         Go();
         GetResult(lj_handle, LJ_ioGET_AIN, 0, &ambientLightSens);
-        GetResult(lj_handle, LJ_ioGET_AIN, 1, &opticalSens);
 
-        // Detect the ambient light sensor to activate the servo
+        // Detect the ambient light sensor to activate the solenoid
         if (ambientLightSens < 1.0 && stickyBit == 0) {
-            count++;
             stickyBit = 1;
-            AddRequest(lj_handle, LJ_ioPUT_DIGITAL_BIT, 4, 1, 0, 0);
+            AddRequest(lj_handle, LJ_ioPUT_DIGITAL_BIT, 5, 1, 0, 0);
             Go();
-        } else {
+        } else if (ambientLightSens > 1.0 && stickyBit == 1) {
+            count++;
             stickyBit = 0;
-            AddRequest(lj_handle, LJ_ioPUT_DIGITAL_BIT, 4, 0, 0, 0);
+            AddRequest(lj_handle, LJ_ioPUT_DIGITAL_BIT, 5, 0, 0, 0);
             Go();
         }
 
-        // Make the red LED blink
-        if (opticalSens > 4.5) {
+        // Get the state of the optical sensor AFTER the solenoid state is changed (and it finishes moving)
+        Sleep(100);
+        AddRequest(lj_handle, LJ_ioGET_AIN, 1, 0, 0, 0);
+        Go();
+        GetResult(lj_handle, LJ_ioGET_AIN, 1, &opticalSens);
+
+        // Make the red LED blink when the optical sensor is not blocked
+        if (opticalSens > 4.0) {
             AddRequest(lj_handle, LJ_ioPUT_TIMER_VALUE, 0, 32768, 0, 0);
             Go();
         } else {
@@ -54,5 +70,14 @@ int main() {
         }
     }
 
+    // Turn off the solenoid, in case it's still on
+    AddRequest(lj_handle, LJ_ioPUT_DIGITAL_BIT, 5, 0, 0, 0);
+    Go();
+
     Close();
 }
+
+/* OUTPUT
+
+Serial number: 320093113
+*/
